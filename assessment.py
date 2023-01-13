bf6631a4255908f03138dc907ad9c1ad6ffe77a6
KelvinOyanna <dotkelplus@gmail.com>
2023-01-13 20:51:19 +0100

    added function to iteratively load data from tables in source database (wpi database) to tables in postgresql database

--- assessment.py
+++ assessment.py
@@ -3,8 +3,21 @@ import pandas as pd
 from sqlalchemy import create_engine
 import os
 
-data_path = f'{os.getcwd()}/data'
 # Create a connection to the Microsoft Access database file
-engine = create_engine("access+pyodbc://@wpi_data")
-wpi_data = pd.read_sql('WPI', engine)
-print(wpi_data.head())
+access_db_engine = create_engine("access+pyodbc://@wpi_data")
+# Create connection to Postgresql database
+postgres_engine = create_engine('postgresql://postgres:root@localhost:5432/kippa_db')
+
+wpi_data_tables = ['WPI Region', 'WPI Import', 'Wpi Data', 'Shelter Afforded LUT', 'Repairs Code LUT', 'Maximum Size Vessel LUT', \
+'Harbor Type LUT', 'Harbor Size LUT', 'Drydock/Marine Railway Code LUT', 'Depth Code LUT', 'Country Codes', 'Country Codes Old']
+
+# Function to load data from access database to postgresql database
+def load_data_to_db():
+    for table_name in wpi_data_tables:
+        table_data = pd.read_sql(table_name, access_db_engine)
+        destination_table_name = '_'.join(table_name.lower().split(' '))
+        table_data.to_sql(destination_table_name, con= postgres_engine, if_exists='replace')
+    print('Data successfully loaded to posgresql database')
+
+
+load_data_to_db()
