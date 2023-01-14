17c4b7d308a67d765ce9213ffd4ac56ebe12a890
KelvinOyanna <dotkelplus@gmail.com>
2023-01-15 00:07:49 +0100

    added jurong_island_nearest_port.py script for creating a table of the 5 nearest ports to Singapore JURONG ISLAND port

--- etl.py
+++ etl.py
@@ -3,25 +3,30 @@ import pandas as pd
 from sqlalchemy import create_engine
 from dotenv import dotenv_values
 dotenv_values()
-import os
 
-# Get database credentials from environment variable
-config = dict(dotenv_values('.env'))
-db_user = config.get('DB_USER')
-db_password = config.get('DB_PASSWORD')
-db_name = config.get('DB_NAME')
-port = config.get('PORT')
+
+def get_database_conn():
+    # Get database credentials from environment variable
+    config = dict(dotenv_values('.env'))
+    db_user = config.get('DB_USER')
+    db_password = config.get('DB_PASSWORD')
+    db_name = config.get('DB_NAME')
+    port = config.get('PORT')
+    # Create connection to Postgresql database
+    return create_engine(f'postgresql://{db_user}:{db_password}@localhost:{port}/{db_name}')
 
 # Create a connection to the Microsoft Access database file
 access_db_engine = create_engine("access+pyodbc://@wpi_data")
-# Create connection to Postgresql database
-postgres_engine = create_engine(f'postgresql://{db_user}:{db_password}@localhost:{port}/{db_name}')
 
 wpi_data_tables = ['WPI Region', 'WPI Import', 'Wpi Data', 'Shelter Afforded LUT', 'Repairs Code LUT', 'Maximum Size Vessel LUT', \
 'Harbor Type LUT', 'Harbor Size LUT', 'Drydock/Marine Railway Code LUT', 'Depth Code LUT', 'Country Codes', 'Country Codes Old']
 
 # Function to load data from access database to postgresql database
 def load_data_to_db():
+    '''
+    Function to load data to database
+    '''
+    postgres_engine = get_database_conn()
     for table_name in wpi_data_tables:
         table_data = pd.read_sql(table_name, access_db_engine)
         # Generate valid table names for destination tables in postgresql
@@ -30,4 +35,9 @@ def load_data_to_db():
     print('Data successfully loaded to posgresql database')
 
 
-#load_data_to_db()
+
+if __name__== '__main__':
+    #load_data_to_db()
+    table_data = pd.read_sql('Country Codes', access_db_engine)
+    #schema = pd.io.sql.get_schema(table_data, 'country_codes')
+    print(table_data.columns)
