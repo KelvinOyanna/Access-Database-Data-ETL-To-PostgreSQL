ed7c01f8353e3de9cd3df7429fde97c8a8f9b25f
KelvinOyanna <dotkelplus@gmail.com>
2023-01-15 17:42:12 +0100

    improve file download function

--- etl.py
+++ etl.py
@@ -7,8 +7,8 @@ import zipfile
 
 def download_data():
     '''
-    This function download the required data for this assessment and extract into the
-    directory named data withing the project folder.
+    This function downloads the required data for this assessment in a zip file and extracts it into the
+    directory named data withing the project directory.
     Parameter: Does not accept any parameter
     Return value: None
     Return type: None
@@ -43,16 +43,3 @@ def load_data_to_db():
         table_data.to_sql(destination_table_name, con= postgres_engine, if_exists='replace')
     print('Data successfully successfully loaded to posgresql database')
 
-download_data()
-
-# def create_table_relationships():
-#     connection = get_database_conn().raw_connection()
-#     cursor = connection.cursor()
-#     query = '''
-#     ALTER TABLE country_codes_old ADD CONSTRAINT country_code_unique UNIQUE ("Country Code");
-#     ALTER TABLE wpi_data ADD FOREIGN KEY ("Wpi_country_code") REFERENCES country_codes_old ("Country Code");
-#     '''
-#     cursor.execute(query)
-    
-    
-# create_table_relationships()
