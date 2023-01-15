0ff94131856f9410a233b4093eea59665dbac046
KelvinOyanna <dotkelplus@gmail.com>
2023-01-15 21:05:36 +0100

    add updated database info

--- util.py
+++ util.py
@@ -14,13 +14,13 @@ def get_database_conn():
     '''
     # Get database credentials from environment variable
     config = dict(dotenv_values('.env'))
-    db_user = config.get('DB_USER')
+    db_user_name = config.get('DB_USER_NAME')
     db_password = config.get('DB_PASSWORD')
     db_name = config.get('DB_NAME')
     port = config.get('PORT')
     host = config.get('HOST')
     # Create and return a postgresql database connection object
-    return create_engine(f'postgresql://{db_user}:{db_password}@{host}:{port}/{db_name}')
+    return create_engine(f'postgresql+psycopg2://{db_user_name}:{db_password}@{host}:{port}/{db_name}')
 
 def compute_distance(latitude_1, longitude_1, latitude_2, longitude_2):
     '''
